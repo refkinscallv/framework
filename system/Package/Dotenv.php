c8ed2b133ff8ad4e0cbd4aaf11c89c5df4e2755d
Callv Group <refkinscallv@gmail.com>
2024-10-26 15:37:02 +0700

    fix(Dotenv) : Default page if .env file is not exist

--- system/Package/Dotenv.php
+++ system/Package/Dotenv.php
@@ -7,7 +7,14 @@
     class Dotenv {
 
         public function set($documentRoot) {
-            PHPDotenv::createImmutable($documentRoot)->load();
+            if(file_exists($documentRoot . "/.env")) {
+                PHPDotenv::createImmutable($documentRoot)->load();
+            } else {
+                $customText = "<b><small>Please run command 'php fw build'</small></b>";
+                include $documentRoot ."/public/views/default.php";
+                die();
+            }
         }
 
     }
+    
