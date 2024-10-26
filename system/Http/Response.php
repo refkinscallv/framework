86a4202999fc4e93238887db5dda9e2af3b2456c
Callv Group <refkinscallv@gmail.com>
2024-10-27 00:20:29 +0700

    fix(Response) : Headers

--- system/Http/Response.php
+++ system/Http/Response.php
@@ -21,14 +21,10 @@
         public function withHeaders($key, $val = null) {
             if (is_array($key)) {
                 foreach ($key as $xKey => $xVal) {
-                    if (!header($xKey . ": " . $xVal)) {
-                        throw new Exception("Failed to set header: " . $xKey);
-                    }
+                    header($xKey . ": " . $xVal);
                 }
             } else {
-                if (!header($key . ": " . $val)) {
-                    throw new Exception("Failed to set header: " . $key);
-                }
+                header($key . ": " . $val);
             }
             return $this;
         }
@@ -46,12 +42,12 @@
         }
 
         public function json($data, $flag = JSON_UNESCAPED_SLASHES) {
-            $this->withHeaders("Content-Type", "application/json");
+            $this->withHeaders("content-type", "application/json");
             echo json_encode($data, $flag);
         }
         
         public function custom($content, $type = "text/plain", $isFile = false) {
-            $this->withHeaders("Content-Type", $type);
+            $this->withHeaders("content-type", $type);
             if (!$isFile) {
                 echo $content;
             } else {
@@ -63,10 +59,8 @@
         }
 
         public function redirect($uri) {
-            if (!filter_var($uri, FILTER_VALIDATE_URL)) {
-                throw new Exception("Invalid URL for redirect: " . $uri);
-            }
             $this->withHeaders("Location", $uri);
             exit();
         }
+        
     }
